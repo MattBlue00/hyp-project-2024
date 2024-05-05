e5a4f0c1013f32d537aa78175a349cecc558517a
manuel-tacca <manuel.tacca@mail.polimi.it>
2024-05-05 18:14:52 +0200

    first try navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,19 +4,69 @@
 
 <template>
   <nav>
-    <NuxtLink to="/">HOME</NuxtLink>
-    <NuxtLink to="/about">ABOUT US</NuxtLink>
-    <NuxtLink to="/persons">ALL PERSONS</NuxtLink>
-    <NuxtLink to="/activities">ALL ACTIVITIES</NuxtLink>
-    <NuxtLink to="/activities/projects">OUR PROJECTS</NuxtLink>
-    <NuxtLink to="/activities/services">OUR SERVICES</NuxtLink>
-    <NuxtLink to="/contact">CONTACTS</NuxtLink>
+    <header class="nav-container">
+        <nav class="nav-group">
+            <div class="logo"><NuxtLink class="nuxt-link" to="/">TEAGREEN</NuxtLink></div>
+            <ul class="nav-links">
+                <li><NuxtLink class="nuxt-link" active-class="active-link" to="/about">ABOUT US</NuxtLink></li>
+                <li><NuxtLink class="nuxt-link" active-class="active-link" to="/persons">OUR TEAM</NuxtLink></li>
+                <li><NuxtLink class="nuxt-link" active-class="active-link" to="/activities">ACTIVITIES</NuxtLink></li>
+                <li><NuxtLink class="nuxt-link" active-class="active-link" to="/activities/projects">PROJECTS</NuxtLink></li>
+                <li><NuxtLink class="nuxt-link" active-class="active-link" to="/activities/services">SERVICES</NuxtLink></li>
+                <li><NuxtLink class="nuxt-link" active-class="active-link" to="/contact">CONTACTS</NuxtLink></li>
+            </ul>
+            <button class="nav-btn" aria-label="toggle menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+        </nav>
+    </header>
   </nav>
 </template>
 
 <style scoped>
-nav {
-  display: flex;
-  gap: 30px;
+
+.nav-container {
+  padding: 0.5rem;
+  top: 0;
+}
+
+.nav-group {
+    margin: 0 auto;
+    display: flex;
+    align-items: baseline;
+    justify-content: space-between;
+}
+
+.logo {
+    font-size: 1.5rem;
+    font-family: Inter;
+}
+
+.nav-links {
+    padding: 0;
+    list-style: none;
+    display: flex;
+    margin: 0;
+    gap: 3.5rem;
+}
+
+.nuxt-link {
+    color: var(--par-color-alt);
+    cursor: pointer;
+}
+
+.nav-btn {
+    display: none;
+}
+.nav-links-mobile {
+    display: none;
+    overflow: hidden;
+}
+
+.active-link {
+    text-decoration: underline;
+    text-underline-offset: 2px;
 }
 </style>
