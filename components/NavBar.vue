be94cbdac3d270ee5599f501d244b7cbd17749e1
manuel-tacca <manuel.tacca@mail.polimi.it>
2024-05-06 12:41:13 +0200

    nav bar: some changes in style

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,63 +2,62 @@
 </script>
 
 <template>
-  <nav>
-    <header class="nav-container">
-      <nav class="nav-group">
-          <div class="logo"><NuxtLink class="nuxt-link" to="/">TEAGREEN</NuxtLink></div>
-          <ul class="nav-links">
-              <li><NuxtLink class="nuxt-link" active-class="active-link" to="/about">ABOUT US</NuxtLink></li>
-              <li><NuxtLink class="nuxt-link" active-class="active-link" to="/persons">OUR TEAM</NuxtLink></li>
-              <li><NuxtLink class="nuxt-link" active-class="active-link" to="/activities">ACTIVITIES</NuxtLink></li>
-              <li><NuxtLink class="nuxt-link" active-class="active-link" to="/activities/projects">PROJECTS</NuxtLink></li>
-              <li><NuxtLink class="nuxt-link" active-class="active-link" to="/activities/services">SERVICES</NuxtLink></li>
-              <li><NuxtLink class="nuxt-link" active-class="active-link" to="/contact">CONTACTS</NuxtLink></li>
-          </ul>
-          <button class="nav-btn" aria-label="toggle menu">
-              <span></span>
-              <span></span>
-              <span></span>
-          </button>
-      </nav>
-    </header>
-  </nav>
+  <header class="nav-container">
+    <nav class="nav-group">
+        <div class="logo"><NuxtLink class="nuxt-link" to="/">TEAGREEN</NuxtLink></div>
+        <ul class="nav-links">
+            <li><NuxtLink class="nuxt-link" active-class="active-link" to="/about">ABOUT US</NuxtLink></li>
+            <li><NuxtLink class="nuxt-link" active-class="active-link" to="/persons">OUR TEAM</NuxtLink></li>
+            <li><NuxtLink class="nuxt-link" active-class="active-link" to="/activities">ACTIVITIES</NuxtLink></li>
+            <li><NuxtLink class="nuxt-link" active-class="active-link" to="/activities/projects">PROJECTS</NuxtLink></li>
+            <li><NuxtLink class="nuxt-link" active-class="active-link" to="/activities/services">SERVICES</NuxtLink></li>
+            <li><NuxtLink class="nuxt-link" active-class="active-link" to="/contact">CONTACTS</NuxtLink></li>
+        </ul>
+    </nav>
+  </header>
 </template>
 
 <style scoped>
 .nav-container {
+  box-sizing: border-box;
   padding: 0.5rem;
-  top: 0;
+  background-color: #DA0E5F;
+  color: #FFFAF1;
 }
 .nav-group {
-    margin: 0 auto;
-    display: flex;
-    align-items: baseline;
-    justify-content: space-between;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 .logo {
-    font-size: 1.5rem;
-    font-family: Inter;
+  font-size: 2.5rem;
+  font-family: Montserrat;
+  font-weight: bold;
+  color: #FFFAF1;
 }
 .nav-links {
-    padding: 0;
-    list-style: none;
-    display: flex;
-    margin: 0;
-    gap: 3.5rem;
+  font-family: Lato;
+  font-size: 1.25rem;
+  padding: 0;
+  display: flex;
+  margin: 0;
+  gap: 3rem;
+  list-style: none;
 }
 .nuxt-link {
-    color: var(--par-color-alt);
-    cursor: pointer;
+  color: #FFFAF1;
+  cursor: pointer;
+  text-decoration: none;
 }
-.nav-btn {
+
+.nuxt-link:visited {
+  color: #FFFAF1;
+}
+
+@media screen and (max-width: 1090px) {
+  .nav-links {
     display: none;
-}
-.nav-links-mobile {
-    display: none;
-    overflow: hidden;
-}
-.active-link {
-    text-decoration: underline;
-    text-underline-offset: 2px;
+  }
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// https://nuxt.com/docs/api/configuratiofn/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase'],
+  modules: ['@nuxtjs/supabase', '@nuxtjs/google-fonts'],
   supabase: {
     redirect: false
+  },
+  googleFonts: {
+    families: {
+        Inter: true,
+        Roboto: true,
+        Montserrat: true,
+        Lato: true,
+    }
   }
 })
