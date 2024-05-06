ce5f05dadfc9da9f49d04b1c349211a97a7e3963
Riccardo Speroni <riccardo.speroni@mail.polimi.it>
2024-05-06 22:27:05 +0200

    ActivityCard, personCard, CVInfoContainer, interactive map
    added activityCard, personCard, CVInfoContainer components and added them to the respect pages, added the stuff to make everything work (api files), added interactive map in the contact page.

---
+++ server/api/person/getPersonById.ts
@@ -0,0 +1,21 @@
+import { serverSupabaseClient } from '#supabase/server';
+import { Database } from '~/types/schema';
+
+/**
+ *  Query that returns the person's information, given its ID.
+ */
+export default eventHandler(async (event) => {
+    // person id from the parameters of the query
+    const { id } = getQuery(event);
+    // client for the supabase DB and infer the DB structure
+    const client = serverSupabaseClient<Database>(event);
+    // query the Db and then send to the Vue client an object with projects and error field
+    const { data, error } = await client.then((c) => c
+        .from('Person')
+        .select('id, bio, education, past_experience, main_expertise, main_role, full_name, motto, picture')
+        .eq('id', id!)
+        .single()
+    );
+    if (error) console.log(error);
+    return data;
+});
