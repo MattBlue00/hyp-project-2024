2374613cfe07b52454ae9ca09ef9f6aab4cac72e
Riccardo Speroni <riccardo.speroni@mail.polimi.it>
2024-05-28 10:19:42 +0200

    added new components to improve the code: ActivityImageAndSupervisorCardContainer and GroupLinksContainer

--- pages/activities/projects/[id].vue
+++ pages/activities/projects/[id].vue
@@ -4,7 +4,9 @@ import type {Project} from '~/types/Project';
 import type {Person} from '~/types/Person';
 import {handleFetchError} from "~/composables/errorHandlers";
 import DescriptionContainer from "~/components/containers/DescriptionContainer.vue";
-import PersonCard from "~/components/cards/PersonCard.vue";
+import ActivityImageAndSupervisorCardContainer
+  from "~/components/containers/ActivityImageAndSupervisorCardContainer.vue";
+import GroupLinksContainer from "~/components/containers/GroupLinksContainer.vue";
 
 useSeoMeta({
   title: 'SheRise | Project',
@@ -12,20 +14,6 @@ useSeoMeta({
 });
 
 const { id } = useRoute().params;
-
-// Computed property per convertire id in numero
-const numericId = computed(() => {
-  return Number(id);
-});
-
-// Computed properties per calcolare prevId e nextId
-const prevId = computed(() => {
-  return numericId.value - 1;
-});
-
-const nextId = computed(() => {
-  return numericId.value + 1;
-});
 
 // fetch the project information
 const {
@@ -65,14 +53,6 @@ if (person_error.value?.statusCode) {
   }
 }
 
-const altPicture = computed(() => {
-  return "Picture of the '" + project.value?.name +"' project";
-});
-
-
-
-// TODO: compute alt supervisor picture
-
 </script>
 
 <template>
@@ -82,37 +62,21 @@ const altPicture = computed(() => {
   <div v-else v-if="project">
     <h1 class="project-title">{{ project.name }}</h1>
 
-    <section class="project-img-and-person-card-container">
-      <div v-if="is_project_loading">
+    <section>
+      <div v-if="is_project_loading || is_person_loading">
         <Loader/>
       </div>
-      <div v-else v-if="project">
-        <nuxt-img class="project-img" :src="project.picture" :alt="altPicture"/>
-      </div>
-      <div v-if="is_person_loading">
-        <Loader/>
-      </div>
-      <div class="supervisor-container" v-else v-if="person">
-        <p class="supervised-by">Supervised By:</p>
-        <NuxtLink class="clickable-card" :to="`/persons/${person?.id}`">
-          <PersonCard :img="person?.picture" :name="person?.full_name" :main_role="person?.main_role" />
-        </NuxtLink>
-      </div>
+      <ActivityImageAndSupervisorCardContainer v-else v-if="project && person" :activity="project" :supervisor="person" />
     </section>
 
     <section>
       <DescriptionContainer :description="project?.description"/>
     </section>
 
-    <section class="group-links">
-      <NuxtLink class="prev-link" v-if="numericId > 1" :to="`/activities/projects/${prevId}`">
-        <p>PREV</p>
-      </NuxtLink>
-      <NuxtLink class="next-link" v-if="numericId < 5" :to="`/activities/projects/${nextId}`">
-        <p>NEXT</p>
-      </NuxtLink>
-      <!-- TODO: remove hardcoded max bound -->
+    <section>
+      <GroupLinksContainer :id="useRoute().params.id" :type="'project'"/>
     </section>
+
   </div>
 </template>
 
@@ -123,62 +87,6 @@ const altPicture = computed(() => {
   justify-content: center;
   margin-top: 2rem;
   margin-bottom: 4rem;
-}
-
-.project-img-and-person-card-container{
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 2rem;
-}
-
-.project-img{
-  display: flex;
-  margin-left: 26rem;
-  width: 40%;
-}
-
-.supervisor-container{
-  display: flex;
-  flex-flow: column;
-  align-items: center;
-  align-content: center;
-  width: 18rem;
-  height: 18.75rem;
-  margin-right: 26rem;
-  background-color: #FFE4EB;
-  padding: 1rem;
-}
-
-.supervised-by{
-  color: #C30753;
-}
-
-.clickable-card{
-  box-shadow: 0rem 0rem 0rem #888888;
-  display: flex;
-  flex-flow: column;
-  align-items: center;
-  align-content: center;
-  text-decoration: none;
-}
-
-.group-links{
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin-top: 3rem;
-  margin-left: 2rem;
-  margin-right: 2rem;
-}
-
-.prev-link{
-  display: flex;
-}
-
-.next-link{
-  display: flex;
 }
 
 </style>
