8f933419988f1ccc01fa1491b361ff5355e2614f
Nume99 <101411518+Nume99@users.noreply.github.com>
2024-06-10 18:01:46 +0200

    Add responsive mobile navbar Modified floating button in order to have always the same dimension

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
+
+// mobile navbar toggle system
+let showMobileNav = ref(false);
+
+const toggleMenu = () => {
+  showMobileNav.value = !showMobileNav.value;
+  document.querySelector('body')?.classList.toggle('disable-scrolling');
+};
+
+const closeMenuOnLink = () => {
+  if (showMobileNav.value === true) {
+    showMobileNav.value = false;
+    document.querySelector('body')?.classList.remove('disable-scrolling');
+  }
+};
+
 </script>
 
 <template>
   <header class="nav-container">
     <nav class="nav-group">
-        <div class="logo"><NuxtLink class="nuxt-link" to="/">SheRise</NuxtLink></div>
+        <div class="logo"><NuxtLink class="nuxt-link" to="/" @click="closeMenuOnLink">SheRise</NuxtLink></div>
         <ul class="nav-links">
             <li><NuxtLink class="nuxt-link" active-class="active-link" to="/about">ABOUT US</NuxtLink></li>
             <li><NuxtLink class="nuxt-link" active-class="active-link" to="/persons">OUR TEAM</NuxtLink></li>
@@ -13,7 +29,23 @@
             <li><NuxtLink class="nuxt-link" active-class="active-link" to="/activities/services">SERVICES</NuxtLink></li>
             <li><NuxtLink class="nuxt-link" active-class="active-link" to="/contact">CONTACTS</NuxtLink></li>
         </ul>
+      <button @click="toggleMenu" class="nav-btn" :class="showMobileNav ? 'close-icon' : ''" aria-label="toggle menu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
     </nav>
+
+    <div class="menu-mobile" :class="showMobileNav === true ? 'open-menu' : 'close-menu'">
+      <ul class="nav-links-mobile">
+        <li><NuxtLink class="nuxt-link" active-class="active-link" to="/about" @click="closeMenuOnLink">ABOUT US</NuxtLink></li>
+        <li><NuxtLink class="nuxt-link" active-class="active-link" to="/persons" @click="closeMenuOnLink">OUT TEAM</NuxtLink></li>
+        <li><NuxtLink class="nuxt-link" active-class="active-link" to="/activities" @click="closeMenuOnLink">ACTIVITIES</NuxtLink></li>
+        <li><NuxtLink class="nuxt-link" active-class="active-link" to="/activities/projects" @click="closeMenuOnLink">PROJECTS</NuxtLink></li>
+        <li><NuxtLink class="nuxt-link" active-class="active-link" to="/activities/services" @click="closeMenuOnLink">SERVICES</NuxtLink></li>
+        <li><NuxtLink class="nuxt-link" active-class="active-link" to="/contact" @click="closeMenuOnLink">CONTACTS</NuxtLink></li>
+      </ul>
+    </div>
   </header>
 </template>
 
@@ -24,6 +56,9 @@
     padding-bottom: 0.4rem;
     background-image: var(--gradient-color);
     color: var(--bg-color);
+    position: sticky;
+    top: 0;
+    z-index: 100;
   }
   .nav-group {
     margin-left: min(5%,10em);
@@ -57,4 +92,110 @@
     color: var(--bg-color);
   }
 
+  /* Mobile responsiveness */
+  /* These three following classes disable mobile menu on larger devices */
+
+  .nav-btn {
+    display: none;
+  }
+  .nav-links-mobile {
+    display: none;
+    overflow: hidden;
+  }
+
+/*  .active-link {
+    text-decoration: underline;
+    text-underline-offset: 2px;
+  }*/
+
+  @media (width < 1000px) {
+    /* These three following classes disable desktop menu on smaller devices */
+    .nav-links {
+      display: none;
+    }
+
+    .nav-group {
+      align-items: center;
+    }
+
+    /* Styling of the mobile menu */
+    .close-menu {
+      transition: 0.3s ease-in all;
+      visibility: hidden;
+      width: 0;
+    }
+
+    .open-menu {
+      transition: 0.3s ease-in all;
+      visibility: visible;
+      width: 100%;
+    }
+
+    .menu-mobile {
+      position: fixed;
+      top: 0;
+      right: 0;
+      z-index: -1;
+      height: 100%;
+      background-image: var(--gradient-color);
+    }
+
+    .nav-links-mobile {
+      margin-top: 3rem;
+      list-style: none;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      gap: 2.5rem;
+      align-items: center;
+      min-height: 60%;
+      padding: 0;
+      font-size: 1.2rem;
+    }
+
+    /* Styling of the mobile-menu button */
+    .nav-btn {
+      display: flex;
+      flex-direction: column;
+      width: 32px;
+      cursor: pointer;
+      background-color: transparent;
+      border: 0;
+      padding: 0;
+    }
+
+    .nav-btn span {
+      background-color: var(--bg-color);
+      height: 3px;
+      margin: 2px 0;
+      transition: 0.4s cubic-bezier(0.68, -0.6, 0.32, 1.6);
+      border-radius: 2px;
+    }
+
+    span:nth-of-type(1) {
+      width: 50%;
+    }
+
+    span:nth-of-type(2) {
+      width: 100%;
+    }
+
+    span:nth-of-type(3) {
+      width: 75%;
+    }
+
+    .close-icon span:nth-child(1) {
+      transform: rotatez(45deg) translate(0, 10px);
+      width: 100%;
+    }
+
+    .close-icon span:nth-child(2) {
+      width: 0;
+    }
+
+    .close-icon span:nth-child(3) {
+      width: 100%;
+      transform: rotatez(-45deg) translate(0, -10px);
+    }
+  }
 </style>
