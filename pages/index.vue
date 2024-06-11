43ae047b2420fbef8c7226483e6885c2fc4e425e
MattBlue00 <matteo2.spreafico@mail.polimi.it>
2024-06-11 17:10:29 +0200

    SEO & Accessibility improvements

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+useSeoMeta({
+  title: 'SheRise | Home',
+  description: 'This is the home page of SheRise with aggregated information about the company\'s activities.',
+});
 </script>
 
 <template>
