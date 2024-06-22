2723f2d9da68a5b4077d95b94099f04380934fc9
Riccardo Speroni <riccardo.speroni@mail.polimi.it>
2024-06-22 15:32:59 +0200

    fixed hardcoded value in getRandomTestimonials Api

--- server/api/testimonial/getRandomTestimonials.ts
+++ server/api/testimonial/getRandomTestimonials.ts
@@ -2,6 +2,8 @@ import { serverSupabaseClient } from '#supabase/server';
 import { Database } from '~/types/schema';
 import {handleMissingDataError} from "~/composables/errorHandlers";
 
+const number_of_witnesses_to_show = 3;
+
 function getRandomNumbers(count: number, min: number, max: number): number[] {
     const numbers: number[] = [];
     while (numbers.length < count) {
@@ -19,8 +21,15 @@ function getRandomNumbers(count: number, min: number, max: number): number[] {
 export default eventHandler(async (event) => {
     // client for the supabase DB and infer the DB structure
     const client = serverSupabaseClient<Database>(event);
+
+    // Query the DB to get the total count of testimonials
+    const { count} = await client.then((c) => c
+        .from('Testimonial')
+        .select('*', { count: 'exact', head: true })
+    );
+
     // query the Db and then send to the Vue client an object with testimonials and error field
-    const randomIds = getRandomNumbers(3, 1, 10);
+    const randomIds = getRandomNumbers(number_of_witnesses_to_show, 1, count!);
     const { data, error } = await client.then((c) => c
         .from('Testimonial')
         .select('id, author_full_name, statement, service_id, author_picture')
